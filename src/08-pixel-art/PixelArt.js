8594c5dbfe8b1c59882061274de47bdea9e76ba8
Conner McKenna <conner.mckenna94@gmail.com>
2022-12-05 11:58:04 -0500

    feat: finishing challenge 08

--- src/08-pixel-art/PixelArt.js
+++ src/08-pixel-art/PixelArt.js
@@ -1,22 +1,52 @@
-import React from 'react'
+import React, { createContext, useContext, useState } from 'react';
+
+const ColorContext = createContext({
+  color: 'lightGrey',
+  setColor: () => {}
+});
+
 
 function ColorPicker () {
-  const colors = ['red', 'blue', 'yellow', 'green', 'black', 'white', 'purple']
+  const colors = ['red', 'blue', 'yellow', 'green', 'black', 'white', 'purple'];
+  const { setColor, color: currentColor } = useContext(ColorContext);
+
   return (
     <div>
       <h1>Choose a color</h1>
-      {colors.map(color => <button key={color} style={{ backgroundColor: color }} />)}
+      {colors.map(color => (
+        <button
+          key={color}
+          onClick={() => setColor(color)}
+          style={{
+            backgroundColor: color,
+            border: currentColor === color ? '1px solid black' : 'none'
+          }}
+        />
+      ))}
     </div>
   )
 }
 
 function Pixel () {
-  return <div style={{ height: '20px', width: '20px', backgroundColor: 'lightGrey', margin: '1px' }} />
+  const { color } = useContext(ColorContext);
+  const [pixelColor, setPixelColor] = useState('lightGrey');
+
+  return (
+    <div
+      onClick={() => setPixelColor(color)}
+      style={{
+        height: '20px',
+        width: '20px',
+        backgroundColor: pixelColor,
+        margin: '1px'
+      }}
+    />
+  );
 }
 
 function Pixels () {
   const pixels = []
-  for (let i = 0; i < 100; i++) pixels.push(<Pixel key={i} />)
+  for (let i = 0; i < 100; i++) pixels.push(<Pixel key={i} />);
   return (
     <div style={{ display: 'grid', gridTemplateColumns: 'repeat(10, 1fr)', width: '210px', margin: '0 auto' }}>
       {pixels}
@@ -25,10 +55,12 @@ function Pixels () {
 }
 
 export default function PixelArt () {
+  const [color, setColor] = useState('lightGrey');
+
   return (
-    <div>
+    <ColorContext.Provider value={{ color, setColor }}>
       <ColorPicker />
       <Pixels />
-    </div>
+    </ColorContext.Provider>
   )
 }
